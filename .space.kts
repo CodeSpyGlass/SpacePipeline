d5f5a560423d86fe0a08ee87ea23e1bbe27dd0ee
romo <robmoore121@gmail.com>
2020-12-31 13:22:36 +0800

    Rename 'link' to 'url'

--- .space.kts
+++ .space.kts
@@ -1,12 +1,12 @@
 job("CodeSpyGlass") {
     container("alpine/git") {
-        env["GITHUB_LINK"] = Params("githublink")
+        env["GITHUB_URL"] = Params("githuburl")
         shellScript {
             content = """
-                echo "Cloning ${'$'}GITHUB_LINK"
+                echo "Cloning ${'$'}GITHUB_URL"
                 git --version
                 rm -rf code
-                git clone ${'$'}GITHUB_LINK code
+                git clone ${'$'}GITHUB_URL code
                 ls -al code
             """
         }
