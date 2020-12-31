0fcaed0e1ca8729a5899890ca2e4d7599e5a2a93
romo <robmoore121@gmail.com>
2020-12-31 15:34:32 +0800

    Remove class noodling with plain functions.

--- .space.kts
+++ .space.kts
@@ -1,61 +1,27 @@
 job("CodeSpyGlass") {
     container("openjdk:11") {
-        class JobEnvironmentVariable(
-            private val environmentVariableReference: String,
-            pipelineParameterName: String
-        ) {
-            init {
-                env[environmentVariableReference] =
-                    Params(pipelineParameterName)
-            }
-
-            /**
-             * A reference which can be used when constructing shell
-             * commands, such as `echo "$MY_ENVIRONMENT_VARIABLE"`
-             *
-             * This method returns "$GITHUB_URL" for the environment variable
-             * "GITHUB_URL".
-             */
-            fun shellReference() = "${'$'}$environmentVariableReference"
-        }
-
-        abstract class PipelineStage {
-            abstract fun run()
-        }
-
-        class CloneStage(
-            private val githubUrl: JobEnvironmentVariable,
-            private val codeDirectory: String
-        ) : PipelineStage() {
-            override fun run() {
-                shellScript {
-                    content = """
-                echo "Cloning '${githubUrl.shellReference()}' into directory '$codeDirectory'"
+        fun cloneStage(githubUrlShellReference: String, codeDirectory: String) {
+            shellScript {
+                content = """
+                echo "Cloning '$githubUrlShellReference' into directory '$codeDirectory'"
                 git --version
                 rm -rf $codeDirectory
-                git clone ${githubUrl.shellReference()} $codeDirectory
+                git clone $githubUrlShellReference $codeDirectory
                 ls -al $codeDirectory
             """.trimIndent()
-                }
             }
         }
 
-        class AnalysisStage(
-            private val codeDirectory: String
-        ) : PipelineStage() {
-            override fun run() {
-                shellScript {
-                    content = """
+        fun analysisStage(codeDirectory: String) {
+            shellScript {
+                content = """
                         echo "Analysing Java in the directory '$codeDirectory'"
                     """.trimIndent()
-                }
             }
         }
 
-        val githubUrl = JobEnvironmentVariable("GITHUB_URL", "githuburl")
-        listOf(
-            CloneStage(githubUrl, "code"),
-            AnalysisStage("code")
-        ).forEach(PipelineStage::run)
+        env["GITHUB_URL"] = Params("githuburl")
+        cloneStage("${'$'}GITHUB_URL", "code")
+        analysisStage("code")
     }
 }
