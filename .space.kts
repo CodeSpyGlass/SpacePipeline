e935edfb7260ac5961ed8ecfa2ed65ffce89b995
romo <robmoore121@gmail.com>
2020-12-31 14:41:46 +0800

    Make a small domain model with some classes and prepare to expand to the other stages.

--- .space.kts
+++ .space.kts
@@ -1,28 +1,47 @@
-class JobEnvironmentVariable(
-    environment: Environment,
-    environmentVariableValue: ParamContext,
-    private val environmentVariableName: String
-) {
-    init {
-        environment[environmentVariableName] = environmentVariableValue
-    }
-
-    fun ref(): String = "${'$'}${environmentVariableName}"
-}
-
 job("CodeSpyGlass") {
     container("alpine/git") {
-        val githubUrl = JobEnvironmentVariable(
-            env, Params("githuburl"), "GITHUB_URL"
-        )
-        shellScript {
-            content = """
-                echo "Cloning ${githubUrl.ref()}"
+        class JobEnvironmentVariable(
+            private val environmentVariableReference: String,
+            pipelineParameterName: String
+        ) {
+            init {
+                env[environmentVariableReference] =
+                    Params(pipelineParameterName)
+            }
+
+            /**
+             * A reference which can be used when constructing shell
+             * commands, such as `echo "$MY_ENVIRONMENT_VARIABLE"`
+             *
+             * This method returns "$GITHUB_URL" for the environment variable
+             * "GITHUB_URL".
+             */
+            fun shellReference() = "${'$'}$environmentVariableReference"
+        }
+
+        abstract class PipelineStage {
+            abstract fun run()
+        }
+
+        class CloneStage(
+            private val githubUrl: JobEnvironmentVariable
+        ) : PipelineStage() {
+            override fun run() {
+                shellScript {
+                    content = """
+                echo "Cloning ${githubUrl.shellReference()}"
                 git --version
                 rm -rf code
-                git clone ${githubUrl.ref()} code
+                git clone ${githubUrl.shellReference()} code
                 ls -al code
             """
+                }
+            }
         }
+
+        val githubUrl = JobEnvironmentVariable("GITHUB_URL", "githuburl")
+        listOf(
+            CloneStage(githubUrl)
+        ).forEach(PipelineStage::run)
     }
 }
