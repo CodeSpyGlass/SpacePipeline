f47da4317bbeb3d80dc1b26099300c8355236e5e
romo <robmoore121@gmail.com>
2020-12-31 13:31:29 +0800

    Add a class to the script

--- .space.kts
+++ .space.kts
@@ -1,13 +1,26 @@
+class JobEnvironmentVariable(
+    environment: Environment,
+    environmentVariableValue: ParamContext,
+    private val environmentVariableName: String
+) {
+    init {
+        environment[environmentVariableName] = environmentVariableValue
+    }
+
+    fun ref(): String = "${'$'}${environmentVariableName}"
+}
+
 job("CodeSpyGlass") {
     container("alpine/git") {
-        env["GITHUB_URL"] = Params("githuburl")
-        val githubUrlEnvironmentVariable = "${'$'}GITHUB_URL"
+        val githubUrlEnvironmentVariableName = "GITHUB_URL"
+        env[githubUrlEnvironmentVariableName] = Params("githuburl")
+        val githubUrlEnvironmentVariableRef = "${'$'}GITHUB_URL"
         shellScript {
             content = """
-                echo "Cloning $githubUrlEnvironmentVariable"
+                echo "Cloning $githubUrlEnvironmentVariableRef"
                 git --version
                 rm -rf code
-                git clone $githubUrlEnvironmentVariable code
+                git clone $githubUrlEnvironmentVariableRef code
                 ls -al code
             """
         }
