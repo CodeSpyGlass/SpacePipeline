ccfcf72e083f3132394ae958302d451b136e9fb4
romo <robmoore121@gmail.com>
2020-12-31 12:54:06 +0800

    Clone the code

--- .space.kts
+++ .space.kts
@@ -5,6 +5,9 @@ job("CodeSpyGlass") {
             content = """
                 echo "Cloning ${'$'}GITHUB_LINK"
                 git --version
+                rm -rf code
+                git clone ${'$'}GITHUB_LINK code
+                ls -al code
             """
         }
     }
